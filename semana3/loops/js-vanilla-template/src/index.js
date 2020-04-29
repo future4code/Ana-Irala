c3755c47af9a5c3b1b7930e6036a1e74e4c9682c
Ana Terra Maraschin Irala <atmirala@gmail.com>
2020-04-29 16:47:45 -0300

    exercicios 1 e 3a e b estão feitos

--- semana3/loops/js-vanilla-template/src/index.js
+++ semana3/loops/js-vanilla-template/src/index.js
@@ -3,7 +3,8 @@ for(let i = 0; i < 15; i++) {
   sum += i
 }
 console.log(sum)
-//
+//O resultado impresso é 105
+//O programa está criando a soma de todos valores de i enquanto eles forem menores que 15
 
 const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
 const novaLista = []
@@ -18,37 +19,32 @@ console.log(novaLista)
 
 
 //exerc.3a
-const array = [2,4,8,33,37,82,7,55,90,94]
+//O maior número é  94 e o menor é  2
+let batatas = [2, 4, 8, 33, 37 ,82, 7, 55, 90 ,94]
+let max = batatas[0]
+let min = batatas[0]
+
+for (let num of batatas) {
+  if (num > max) {
+      max = num
+  }
+}
+
+for (let num of batatas) {
+  if (num <min) {
+      min = num
+  }
+}
+console.log ("O maior número é", max, "e o menor é", min)
 
 
+//exerc.3b
+//O resultado foi [0.2, 0.4, 0.8, 3.3, 3.7, 8.2, 0.7, 5.5, 9, 9.4]
+let batatas = [2, 4, 8, 33, 37 ,82, 7, 55, 90 ,94]
+novasBatatas = []
 
-
-
-
-
-
-// Este array será usado para exemplificar as respostas de todos os itens
-const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
-
-// Resposta a.
-"O maior número é 130 e o menor é 21"
-
-// Resposta item b.
-[8, 3, 13, 4, 6, 2.1, 7, 12, 9, 10.3, 11, 5.5]
-
-// Resposta item c.
-[80, 30, 130, 40, 60, 70, 120, 90, 110] 
-
-// Resposta item d.
-[ 'O elemento do índex 0 é 80',
-  'O elemento do índex 1 é 30',
-  'O elemento do índex 2 é 130',
-  'O elemento do índex 3 é 40',
-  'O elemento do índex 4 é 60',
-  'O elemento do índex 5 é 21',
-  'O elemento do índex 6 é 70',
-  'O elemento do índex 7 é 120',
-  'O elemento do índex 8 é 90',
-  'O elemento do índex 9 é 103',
-  'O elemento do índex 10 é 110',
-  'O elemento do índex 11 é 55' 
+for(num of batatas) {
+  multiDez = num/10
+  novasBatatas.push(multiDez)
+}
+ console.log (novasBatatas)
