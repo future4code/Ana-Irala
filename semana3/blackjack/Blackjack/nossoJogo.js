1c59c22ff33eec2d0e0eebd476ec00802dd88292
Ana Terra Maraschin Irala <atmirala@gmail.com>
2020-04-30 16:53:22 -0300

    entrega obrigatório do fim do dia

--- semana3/blackjack/Blackjack/nossoJogo.js
+++ semana3/blackjack/Blackjack/nossoJogo.js
@@ -1,12 +1,17 @@
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-    
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
+alert("Bem vindo ao jogo de Blackjack!")
+prompt("Deseja iniciar uma jogada")
+
+
+
+
+
+cost cartas = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+const naipes = ["♦️", "♥️", "♣️", "♠️"]
+
+if (numero === "A") {
+   valor = 11
+ } else if (numero === "J" || numero === "Q" || numero === "K") {
+   valor = 10
+ } else {
+   valor = Number(numero)
+ }
