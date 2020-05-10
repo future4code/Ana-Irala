b6597201f3a1b71c0eef5ddc1dff9d98e030ec79
Ana Terra Maraschin Irala <atmirala@gmail.com>
2020-05-10 19:21:12 -0300

    add exercícios 3 de lógica de programação e 2 de funções

--- semana4/lista-de-exercicios/lista.js
+++ semana4/lista-de-exercicios/lista.js
@@ -13,17 +13,54 @@ Foi feita uma função que possui um argumento valorEmDolar.
 
  /*Exercicio2
  Temos uma função investeDinheiro, com os argumentos tipoDeInvestimento e valor. 
- Na funçao, usamos a condicional switch case pra dizer o que será feito com cada tipo de argumento tipoDeInvestimento. 
+ Na funçao, usamos a condicional switch case pra dizer o que será feito com cada valor de argumento tipoDeInvestimento. 
  Por exemplo, no caso do argumento escolhido pelo usuário ser poupança, a variável valorAposInvestimento será 
  o parametro valor dado pelo usuário vezes 1.03.
  No caso do tipoDeInvestimento escolhido pelo usuário náo se encaixar em nenhum case, ele cairá no default e um alert surgirá
  dizendo que o tipo de investimento informado está incorreto. 
  Ao fim da função, retornará o valorAposInvestimento.
- Fora do escopo da função, duas variáveis const : novoMontante, que chama a função e define os argumentos como "Ações"(tipoDeInvestimento) e 150 (valor);
- A outra variável, segundoMontante chama a função e define os argumentos como "Tesouro Direto" e 200
- Ao dar console.log(novoMontante), aparecerá no console 165.
- Ao dar console.log(segundoMontante), uma caixa de alerta será mostrada com a mensagem "TIPO DE INVESTIMENTO INFORMADO INCORRETO!"
+ Fora do escopo da função, duas variáveis const : novoMontante, que chama a função e passa os argumentos como "Ações"(tipoDeInvestimento) e 150 (valor);
+ A outra variável, segundoMontante chama a função e passa os argumentos como "Tesouro Direto" e 200
+ Ao executar console.log(novoMontante), aparecerá no console 165.
+ Ao executar console.log(segundoMontante), uma caixa de alerta será mostrada com a mensagem "TIPO DE INVESTIMENTO INFORMADO INCORRETO!"
  */
 
  /* Exercício3
- 
+
+
+
+
+
+
+//LOGICA DE PROGRAMACAO
+
+/*Exercício3
+O código original não funcionava por algumas razões... quantidadeDeNumerosPares não estava declarado
+nem tinha informação alguma;
+while tinha que ter como parametro i < e não i<=, pois começa com zero e colocando o igual, vai retornar um
+valor a mais do que queremos. Por exemplo, mantendo o <=, se dermos o valor de 3 para o quantidadeDeNumerosPares,
+ao inves de retornar no console o esperado (0, 2, 4), irá aparecer 0, 2, 4, 6.
+Além disso, faltava implementar o i++, que indica pro nosso código que queremos adicionar 1
+ao i, cada vez que ele rodar (enquanto - while- i for menor que o valor de quantidadeDeNumerosPares)
+Em seguida, temos o código corrigido e testado!*/
+const quantidadeDeNumerosPares = Number(prompt("Digite a quantidade de números pares que deseja."));
+let i = 0
+while(i < quantidadeDeNumerosPares) {
+console.log(i*2);
+i++;
+}
+
+
+
+
+
+//FUNÇÕES
+
+/*Exercício2*/
+
+function alertaFuture () {
+     alert("Hello Future4!");
+     return;
+ }
+
+alertaFuture ();
