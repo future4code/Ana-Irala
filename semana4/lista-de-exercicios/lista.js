7af24461e25210ce737e1cf217ee38b59fa7f8c2
Ana Terra Maraschin Irala <atmirala@gmail.com>
2020-05-11 03:34:59 -0300

    exercicio3 de funções adicionado

--- semana4/lista-de-exercicios/lista.js
+++ semana4/lista-de-exercicios/lista.js
@@ -229,6 +229,24 @@ function criaRetangulo (lado1, lado2) {
     return retangulitos
 }
 
+/*Exercicio3
+3. Crie um objeto para representar seu filme favorito. 
+Ele deve ter as seguintes propriedades: título, ano, diretor e 
+atores/atrizes (lista com pelo menos 2 atores/atrizes). 
+Imprima na tela a seguinte string, baseada nos valores do objeto:
+`Venha assistir ao filme NOME DO FILME, de ANO, dirigido por DIRETOR e 
+estrelado por ATOR 1, ATRIZ 2, ATOR n`. 
+A lista de atores/atrizes deve ser impressa inteira, independente do tamanho da lista.
+*/
+let filmeFavorito = {
+    titulo: "Mulholland Drive",
+    ano: 2001,
+    diretor: "David Lynch",
+    atrizesAtores: ["Naomi Watts", "Laura Harring", "Justin Theroux"],
+}
+
+console.log(`Venha assistir ao filme ${filmeFavorito.titulo}, de ${filmeFavorito.ano}, dirigido por ${filmeFavorito.diretor} e estrelado por ${filmeFavorito.atrizesAtores}.`)
+
 /*Exercício4*/
 let perfilPessoa = {
     nome: "Fulano da Silva",
