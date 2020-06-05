ffb1b58619cd684e82e3834f901a20172087a239
Ana Terra Maraschin Irala <atmirala@gmail.com>
2020-06-05 16:08:08 -0300

    primeiro componente

--- semana7/projeto/labefy/src/App.js
+++ semana7/projeto/labefy/src/App.js
@@ -1,37 +1,15 @@
 import React from 'react';
 import axios from 'axios';
+import Playlists from './components/Playlists'
 
 
 export default class App extends React.Component {
  
-  state = {
-    name: ""
-  };
   
-  handlePlaylistChange = event => {
-    
-    const newPlaylistValue = event.target.value;
-    this.setState({ name: newPlaylistValue });
-  };
- 
-  handleCreatePlaylist = event => {
-
-  };
-  
-
   render () {
     return (
-      <div>
-        <h1>LABEFY</h1>;
-        <input 
-          type="text"
-          value={this.state.playlist}
-          onChange={this.handlePlaylistChange}
-          placeholder= {'Nome da playlist'}>
-        </input>;
-        <button onClick={this.handleCreatePlaylist}>CRIAR PLAYLIST</button>;
-      </div>
-    );
+      <Playlists />
+    )
   }
    
 }
--- semana7/projeto/labefy/src/components/Playlists.js
+++ semana7/projeto/labefy/src/components/Playlists.js
@@ -1,8 +1,41 @@
 import React from 'react';
-import App from './App';
+
 
 class Playlists extends React.Component {
+    state = {
+        name: ""
+      };
+      
+      handlePlaylistChange = event => {
+        
+        const newPlaylistValue = event.target.value;
+        this.setState({ name: newPlaylistValue });
+      };
+     
+      handleCreatePlaylist = event => { //aqui vai o axios post
+    
+      };
 
+   
+   
+   
+   
+   
+   
+    render() {
+        return (
+            <div>
+              <h1>LABEFY</h1>;
+              <input 
+                type="text"
+                value={this.state.playlist}
+                onChange={this.handlePlaylistChange}
+                placeholder= {'Nome da playlist'}>
+              </input>;
+              <button onClick={this.handleCreatePlaylist}>CRIAR PLAYLIST</button>;
+            </div>
+          );
+    }
 }
 
 export default Playlists;
