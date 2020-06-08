98f42f23ca219de6de7b7c40f941a43add8fddc1
Ana Terra Maraschin Irala <atmirala@gmail.com>
2020-06-08 01:39:27 -0300

    playlists listadas

--- semana7/projeto/labefy/src/components/Playlists.js
+++ semana7/projeto/labefy/src/components/Playlists.js
@@ -3,54 +3,83 @@ import axios from 'axios';
 
 class Playlists extends React.Component {
     state = {
-        name: ""
-      };
+        name: "",
+        playlists:[]
+    };
       
-      handleNameChange = event => {
-        
+    handleNameChange = event => {
         const newPlaylistValue = event.target.value;
         this.setState({ name: newPlaylistValue });
-      };
+    };
 
-
-      handleCreatePlaylist = event => { 
-          const axiosConfig = {
-              headers: {
-                Authorization: "ana-irala-mello"
-                }
-            };
+    handleCreatePlaylist = event => { 
+        const axiosConfig = {
+            headers: {
+                Authorization: 'ana-irala-mello'
+            }
+        };
         
-          const body = {
+        const body = {
             name: this.state.name
-          };
-
-          axios.post(
-             'https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists' ,
-              body,
-              axiosConfig
-            ).then(() => {
-                 alert(`Playlist ${this.state.name} criada com sucesso!`);
-                 this.setState({ name: ""});
-                 console.log('deu certo!'); //teste
-            }).catch(error => {
-                 console.log('erro!'); 
-                 alert("Erro ao criar o playlist");
-                });
         };
 
+        axios.post(
+            'https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists' ,
+            body,
+            axiosConfig
+        )
+        .then(() => {
+            alert(`Playlist ${this.state.name} criada com sucesso!`);
+            this.setState({ name: ''});
+            console.log('deu certo!'); //teste
+        })
+        .catch(error => {
+            console.log('erro!'); 
+            alert('Erro ao criar o playlist');
+        });
+    };
+
+    componentDidMount = () => {
+        this.getPlaylist();
+    };
+        
+    getPlaylist = () => {
+        axios.get(
+            'https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists',
+            {
+                headers: {
+                    Authorization: 'ana-irala-mello'
+                }
+            }
+        )
+        .then(answer => {
+            this.setState({ playlists: answer.data.result.list });
+        })
+        .catch(error => {
+            console.log('erro!');
+        });
+    };
+        
     render() {
         return (
             <div>
-              <h1>LABEFY</h1>;
-              <input 
-                type="text"
-                value={this.state.name}
-                onChange={this.handleNameChange}
-                placeholder= {'Nome da playlist'}>
-              </input>;
-              <button onClick={this.handleCreatePlaylist}>CRIAR PLAYLIST</button>;
+                <h1>LABEFY</h1>;
+                <div>
+                    <input 
+                        type="text"
+                        value={this.state.name}
+                        onChange={this.handleNameChange}
+                        placeholder= {'Nome da playlist'}>
+                    </input>;
+                    <button onClick={this.handleCreatePlaylist}>CRIAR PLAYLIST</button>;
+                </div>
+                <div>
+                    {this.state.playlists.map(playlist => {
+                        return <p key={playlist.id}>{playlist.name}</p>;
+                    })}
+                </div>
             </div>
-          );
+        );
     }
 }
 
