8ea8fda458e78b2afff348f671803bcdefb64048
Ana Terra Maraschin Irala <atmirala@gmail.com>
2020-06-08 01:08:34 -0300

    problema com bad request resolvido

--- semana7/projeto/labefy/src/components/Playlists.js
+++ semana7/projeto/labefy/src/components/Playlists.js
@@ -6,7 +6,7 @@ class Playlists extends React.Component {
         name: ""
       };
       
-      handlePlaylistChange = event => {
+      handleNameChange = event => {
         
         const newPlaylistValue = event.target.value;
         this.setState({ name: newPlaylistValue });
@@ -14,23 +14,27 @@ class Playlists extends React.Component {
 
 
       handleCreatePlaylist = event => { 
-          
-          const headers= {
-            Authorization: "ana-irala-mello"
-          };
-
+          const axiosConfig = {
+              headers: {
+                Authorization: "ana-irala-mello"
+                }
+            };
+        
           const body = {
-            name: this.state.name,
+            name: this.state.name
           };
 
           axios.post(
-            'https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists' ,
-             headers ,
-             body
+             'https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists' ,
+              body,
+              axiosConfig
             ).then(() => {
-                 console.log('deu certo!') //teste
+                 alert(`Playlist ${this.state.name} criada com sucesso!`);
+                 this.setState({ name: ""});
+                 console.log('deu certo!'); //teste
             }).catch(error => {
-                 console.log('erro!');
+                 console.log('erro!'); 
+                 alert("Erro ao criar o playlist");
                 });
         };
 
@@ -40,8 +44,8 @@ class Playlists extends React.Component {
               <h1>LABEFY</h1>;
               <input 
                 type="text"
-                value={this.state.playlist}
-                onChange={this.handlePlaylistChange}
+                value={this.state.name}
+                onChange={this.handleNameChange}
                 placeholder= {'Nome da playlist'}>
               </input>;
               <button onClick={this.handleCreatePlaylist}>CRIAR PLAYLIST</button>;
