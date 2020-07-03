62a4a92f7e2de1e382e2f90b29dac287c1b4de7c
atmirala <atmirala@gmail.com>
2020-07-03 13:26:09 -0300

    teste de renderizaçao

--- semana11/planner/src/App.test.js
+++ semana11/planner/src/App.test.js
@@ -1,9 +1,37 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render} from "@testing-library/react";
+import App from "./App";
+import axios from "axios";
+import "@testing-library/jest-dom/extend-expect";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Renderizacao inicial', () => {
+
+  test('Renderiza tudo corretamente', async () => {
+    axios.get = jest.fn().mockResolvedValue({
+      data: [
+        {
+          "text": "testando renderizar",
+          "day": "1",
+          "id": "sYLgZCjyXTwr3G9lqNIP"
+        }
+      ]
+    })
+
+    const {
+      getByPlaceholderText,
+      findByText,
+      getByText
+    } = render(<App />)
+
+    const input = getByPlaceholderText('Nova tarefa')
+    expect(input).toBeInTheDocument()
+
+    expect(getByText(/Criar/)).toBeInTheDocument()
+
+    expect(axios.get).toHaveBeenCalled()
+
+    const tarefa = await findByText("testando renderizar")
+
+    expect(tarefa).toBeInTheDocument()
+  })
+})
