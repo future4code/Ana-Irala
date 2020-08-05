41e65ed0f48fe926d8d66fbea51db4796e602b84
atmirala <atmirala@gmail.com>
2020-08-04 23:37:28 -0300

    one

--- semana15/aula2/src/index.ts
+++ semana15/aula2/src/index.ts
@@ -1,4 +1,10 @@
-// import {Customer} from "./Customer";
+import {Customer} from "./Customer";
 // import {Employee} from "./Employee";
 // import {Seller} from "./Salesman";
 // import {User} from "./User";
+
+
+
+const oneCustomer = new Customer("id666", "coquinho@email.com", "Zé do Coquinho"," 123456", "1111222233334444");
+
+console.log(oneCustomer);
